12c9b995d452a064160a844ad3ba5d98dcd9fc39
Adam Sandler <SokovianSquirrelly>
2023-02-24 18:35:00 -0700

    added memes

--- meme-shuffler.js
+++ meme-shuffler.js
@@ -1,11 +1,26 @@
 let memeSources = [
+    "memes/abbathMeme.jpg",
+    "memes/blkMeme.jpg",
+    "memes/cntryMeme.jpg",
+    "memes/dthcrMeme.jpg",
     "memes/hvyMeme_1.jpg",
     "memes/prgMeme_1.JPG",
     "memes/hvyMeme_2.JPG",
     "memes/hvyMeme_3.JPG",
-    "memes/gshsmk.JPG",
+    "memes/hvyMeme_4.jpg",
+    "memes/hvyMeme_5.jpg",
+    "memes/hvyMeme_6.jpg",
+    "memes/godsmackMeme_1.JPG",
+    "memes/godsmackMeme_2.jpg",
     "memes/kornMeme.jpg",
-    "memes/rammsteinMeme.JPG"
+    "memes/linkinParkMeme.jpg",
+    "memes/lornaShoreMeme.jpg",
+    "memes/metallicaMeme_1.jpg",
+    "memes/metallicaMeme_2.jpg",
+    "memes/pnkMeme.jpg",
+    "memes/rammsteinMeme.JPG",
+    "memes/ratmMeme.jpg",
+    "memes/slipknotMeme.jpg"
 ];
 
 function displayNewMeme()
